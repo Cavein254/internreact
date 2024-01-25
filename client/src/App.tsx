08a5001b887742d0bb530d4dd3f63bcfcc5e0e7d
johnmuthua <caveomuthama@gmail.com>
2024-01-25 13:56:47 +0300

    Add hero section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
 import Services from "./components/services/Services";
 import Subscribe from "./components/subscribe/Subscribe";
+import Team from "./components/team/Team";
 import Testimonials from "./components/testimonial/Testimonials";
 
 function App() {
   return (
     <>
       <div>
+        <Hero />
         <About />
         <Services />
         <Testimonials />
+        <Team />
         <Subscribe />
         <Footer />
       </div>
--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -1,8 +1,7 @@
-import { Button, Typography } from '@mui/material';
-import { Box } from '@mui/system';
-import Image from 'next/image';
-import Link from 'next/link';
-import './style.css';
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import "./style.css";
+import HeroPic from "../../assets/hero.jpg";
 
 const Hero = () => {
   return (
@@ -12,49 +11,43 @@ const Hero = () => {
           <Typography
             variant="h1"
             sx={{
-              fontFamily: 'Merriweather',
-              fontSize: '3rem',
-              lineHeight: '0.85',
-              fontWeight: '400',
+              fontFamily: "Merriweather",
+              fontSize: "3rem",
+              lineHeight: "0.85",
+              fontWeight: "400",
             }}
           >
-            Empower Your Career Journey with{' '}
+            Empower Your Career Journey with{" "}
             <span className="gpol">INTERN CONNECT</span>
           </Typography>
           <Typography
             variant="h5"
             sx={{
-              fontFamily: 'Dancing Script',
-              fontSize: '2rem',
+              fontFamily: "Dancing Script",
+              fontSize: "2rem",
             }}
           >
             Your Success, Our Priority.
           </Typography>
         </Box>
         <Box>
-          <Link href="#services">
+          <a href="#services">
             <Button
               variant="contained"
               sx={{
-                fontFamily: 'Poppins',
-                backgroundColor: '#f60',
-                fontSize: '1.2rem',
+                fontFamily: "Poppins",
+                backgroundColor: "#f60",
+                fontSize: "1.2rem",
                 borderRadius: 0,
               }}
             >
               Discover Our Services
             </Button>
-          </Link>
+          </a>
         </Box>
       </Box>
       <Box className="hero-img-container">
-        <Image
-          height={300}
-          width={300}
-          src="/images/hero.jpg"
-          alt="Hero"
-          className="hero-img"
-        />
+        <img src={HeroPic} alt="Hero" className="hero-img" />
       </Box>
     </Box>
   );
