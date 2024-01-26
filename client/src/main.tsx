ff9a1ae0ae2a152962e061d30b78ba257757c1a9
johnmuthua <caveomuthama@gmail.com>
2024-01-26 20:07:16 +0300

    Add router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./pages/MainPage.tsx";
+import AuthPage from "./pages/AuthPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "auth",
+    element: <AuthPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
