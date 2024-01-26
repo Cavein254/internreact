711348a7f04fe293e94dd1556d3d07ead6ae1dbe
johnmuthua <caveomuthama@gmail.com>
2024-01-26 15:41:10 +0300

    Add slider

--- client/src/components/carousel/Carousel.tsx
+++ client/src/components/carousel/Carousel.tsx
@@ -1,36 +1,30 @@
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Box } from "@mui/material";
 
 const Carousel = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
-    speed: 500,
+    speed: 100,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
   return (
-    <div>
+    <Box>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        <Box>
+          <img src="https://images.unsplash.com/photo-1624555130882-dcfa8ecb17ce?q=80&w=450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </Box>
+        <Box>
+          <img src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </Box>
+        <Box>
+          <img src="https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?q=80&w=450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </Box>
       </Slider>
-    </div>
+    </Box>
   );
 };
 
