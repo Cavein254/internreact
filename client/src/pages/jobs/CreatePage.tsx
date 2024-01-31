256c2f2a6d2b8d4dfa7485045fe130e6e08cce47
johnmuthua <caveomuthama@gmail.com>
2024-01-31 13:42:53 +0300

    Add routes

---
+++ client/src/pages/jobs/CreatePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateJob from "../../components/dashboard/job/CreateJob";
+
+const CreatePage = () => {
+  return (
+    <div>
+      <CreateJob />
+    </div>
+  );
+};
+
+export default CreatePage;
