996dafa07e5ef563ecda9fa694154adf74e476b7
johnmuthua <caveomuthama@gmail.com>
2024-02-03 12:13:02 +0300

    chore: clean up

--- server/controllers/JobController.js
+++ server/controllers/JobController.js
@@ -1,13 +1,9 @@
 export const createJob = async (req, res) => {
   const data = await req.body;
-  console.log("on route");
-  console.log(data);
   try {
     const response = await prisma.job.create({
       data,
     });
-    console.log("000000000000");
-    console.log(response);
     return res.status(200).json(response);
   } catch (err) {
     console.log(err);
